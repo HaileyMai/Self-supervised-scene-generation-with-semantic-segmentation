4ad37199c32283e997421f87b269d75514665f1f
HaileyMai <maiyixin716@163.com>
2022-06-25 01:20:42 +0200

    generate images for evaluation

--- torch/test_scene.py
+++ torch/test_scene.py
@@ -6,6 +6,7 @@ import os, sys
 import shutil
 import random
 import torch
+import torch.nn.functional as F
 import numpy as np
 import gc
 
@@ -13,6 +14,9 @@ import data_util
 import scene_dataloader
 import model as model_util
 import loss as loss_util
+from utils.raycast_rgbd.raycast_rgbd import RaycastRGBD
+import style
+
 
 COLOR_SPACES = ['rgb', 'lab']
 
@@ -40,7 +44,6 @@ parser.add_argument('--vis_only', dest='vis_only', action='store_true')
 parser.add_argument('--weight_color_loss', type=float, default=1.0, help='weight color loss vs rest (0 to disable).')
 parser.add_argument('--weight_semantic_loss', type=float, default=1.0,
                     help='weight semantic loss vs rest (0 to disable).')
-parser.add_argument('--weight_semantic_class', type=float, default=None)  # TODO
 parser.add_argument('--color_thresh', type=float, default=15.0, help='mask colors with all values < color_thresh')
 parser.add_argument('--color_truncation', type=float, default=0, help='truncation in voxels for color')
 parser.add_argument('--augment_rgb_scaling', dest='augment_rgb_scaling', action='store_true')
@@ -48,11 +51,16 @@ parser.add_argument('--augment_scale_min', type=float, default=0.5, help='for co
 parser.add_argument('--augment_scale_max', type=float, default=1.5, help='for color augmentation')
 parser.add_argument('--color_space', type=str, default='lab', help='[rgb, lab]')
 parser.add_argument('--cpu', dest='cpu', action='store_true')
+# 2d proj part
+parser.add_argument('--voxelsize', type=float, default=0.02, help='voxel size in meters.')
+parser.add_argument('--style_width', type=int, default=640, help='width of input for 2d style')
+parser.add_argument('--style_height', type=int, default=512, help='height of input for 2d style')
+
 
 parser.set_defaults(vis_only=False, augment_rgb_scaling=False, cpu=False, pass_geo_feats=True)
 args = parser.parse_args()
 if args.input_dim == 0:  # set default values
-    args.input_dim = (128, 64, 64)
+    args.input_dim = (128, 260, 328)
 args.input_nf = 4
 UP_AXIS = 0  # z is 0th
 print(args)
@@ -71,8 +79,29 @@ model = model_util.Generator(nf_in_geo=1, nf_in_color=nf_in_color, nf=args.nf_ge
 checkpoint = torch.load(args.model_path)
 model.load_state_dict(checkpoint['state_dict'])
 print('loaded model:', args.model_path)
+
+# raycaster
+ray_increment = 0.3 * args.truncation
+thresh_sample_dist = 50.5 * ray_increment
+max_num_locs_per_sample = 1100000  # too high: run out of memory, too low: can only raycast lower locs
+raycast_depth_max = 6.0
+raycaster_rgbd = RaycastRGBD(1, args.input_dim, width=args.style_width, height=args.style_height,
+                             depth_min=0.1 / args.voxelsize, depth_max=raycast_depth_max / args.voxelsize,
+                             thresh_sample_dist=thresh_sample_dist, ray_increment=ray_increment,
+                             max_num_locs_per_sample=max_num_locs_per_sample)
+
+# specify camera intrinsic and extrinsic (R looking down, t will be added later)
+intrinsics = torch.tensor([[269.1120, 269.2970, 320, 256]])
+camera_pose = torch.eye(4)
+camera_pose[:3, 0] = torch.tensor([1, 0, 0])
+camera_pose[:3, 1] = torch.tensor([0, -1, 0])
+camera_pose[:3, 2] = torch.tensor([0, 0, -1])
+camera_pose = camera_pose[None, :]
+
 if not args.cpu:
     model = model.cuda()
+    intrinsics = intrinsics.cuda()
+    camera_pose = camera_pose.cuda()
 
 
 def test(dataloader, output_vis, num_to_vis):
@@ -88,14 +117,12 @@ def test(dataloader, output_vis, num_to_vis):
             mask = sample['mask']
 
             sdfs = sample['sdf']
-            colors = sample['colors'].cuda()
-            target_for_semantics = sample['semantics']
-            target_for_sdf = data_util.preprocess_sdf_pt(sdfs.cuda(), args.truncation)
-            target_for_colors = colors
-            if target_for_semantics is not None:
-                target_for_semantics = target_for_semantics.numpy()
+            colors = sample['colors']
+            semantics = sample['semantics']
+            sdfs = data_util.preprocess_sdf_pt(sdfs.cuda(), args.truncation)
 
             max_input_dim = np.array(inputs.shape[2:])
+            # truncate height in order to view from above
             if args.max_input_height > 0 and max_input_dim[UP_AXIS] > args.max_input_height:
                 max_input_dim[UP_AXIS] = args.max_input_height
                 mask_input = inputs[0][:, UP_AXIS] < args.max_input_height
@@ -105,7 +132,7 @@ def test(dataloader, output_vis, num_to_vis):
             max_input_dim = ((max_input_dim + hierarchy_factor - 1) // hierarchy_factor) * hierarchy_factor
             sys.stdout.write('\r[ %d | %d ] %s (%d, %d, %d)    ' % (
                 num_proc, args.max_to_process, sample['name'], max_input_dim[0], max_input_dim[1], max_input_dim[2]))
-            # pad target to max_input_dim
+            # pad input and target to max_input_dim
             padded = torch.zeros(1, inputs.shape[1], max_input_dim[0], max_input_dim[1], max_input_dim[2])
             padded[:, 0].fill_(-args.truncation)
             padded[:, :, :min(args.max_input_height, inputs.shape[2]), :inputs.shape[3], :inputs.shape[4]] = \
@@ -115,6 +142,20 @@ def test(dataloader, output_vis, num_to_vis):
             padded_mask[:, :, :min(args.max_input_height, mask.shape[2]), :mask.shape[3], :mask.shape[4]] = \
                 mask[:, :, :args.max_input_height, :, :]
             mask = padded_mask
+            target_for_sdfs = torch.zeros(1, 1, max_input_dim[0], max_input_dim[1], max_input_dim[2])
+            target_for_sdfs[:, 0].fill_(-args.truncation)
+            target_for_sdfs[:, :, :min(args.max_input_height, sdfs.shape[2]), :sdfs.shape[3], :sdfs.shape[4]] = \
+                sdfs[:, :, :args.max_input_height, :, :]
+            target_for_sdfs = target_for_sdfs.cuda()
+            target_for_colors = torch.zeros(1, max_input_dim[0], max_input_dim[1], max_input_dim[2], 3)
+            target_for_colors[:, :min(args.max_input_height, colors.shape[1]), :colors.shape[2], :colors.shape[3], :] = \
+                colors[:, :args.max_input_height, :, :, :]
+            target_for_colors = target_for_colors.cuda()
+            target_for_semantics = 41 * torch.ones(1, 1, max_input_dim[0], max_input_dim[1], max_input_dim[2])
+            if semantics is not None:
+                target_for_semantics[:, :, :min(args.max_input_height, semantics.shape[2]), :semantics.shape[3], :semantics.shape[4]] = \
+                semantics[:, :, :args.max_input_height, :, :]
+            target_for_semantics = target_for_semantics.cuda()
 
             model.update_sizes(max_input_dim)
             output_occ = None
@@ -126,19 +167,18 @@ def test(dataloader, output_vis, num_to_vis):
                     inputs, mask, pred_sdf=[True, True], pred_color=args.weight_color_loss > 0,
                     pred_semantic=args.weight_semantic_loss > 0)
 
-                if output_occ is not None:
-                    occ = torch.nn.Sigmoid()(output_occ.detach()) > 0.5
-                    locs = torch.nonzero((torch.abs(output_sdf.detach()[:, 0]) < args.truncation) & occ[:, 0]).cpu()
-                else:
-                    locs = torch.nonzero(torch.abs(output_sdf[:, 0]) < args.truncation).cpu()
+                # if output_occ is not None:
+                #     occ = torch.nn.Sigmoid()(output_occ.detach()) > 0.5
+                #     locs = torch.nonzero((torch.abs(output_sdf.detach()[:, 0]) < args.truncation) & occ[:, 0]).cpu()
+                # else:
+                locs = torch.nonzero(torch.abs(output_sdf[:, 0]) < args.truncation).cpu()
                 locs = torch.cat([locs[:, 1:], locs[:, :1]], 1)
-                output_sdf = [locs, output_sdf[locs[:, -1], :, locs[:, 0], locs[:, 1], locs[:, 2]].detach().cpu()]
+                output_sdf = [locs, output_sdf[locs[:, -1], :, locs[:, 0], locs[:, 1], locs[:, 2]]]
                 if args.weight_color_loss == 0:
                     output_color = None
                 else:
-                    output_color = [locs, output_color[locs[:, -1], :, locs[:, 0], locs[:, 1], locs[:, 2]]]
-                if output_color is not None:
-                    output_color = (output_color[1] + 1) * 0.5
+                    output_color = output_color[locs[:, -1], :, locs[:, 0], locs[:, 1], locs[:, 2]]
+                    output_color = (output_color + 1) * 0.5
                 if args.weight_semantic_loss == 0:
                     output_semantic = None
                 else:
@@ -150,6 +190,90 @@ def test(dataloader, output_vis, num_to_vis):
 
             num_proc += 1
             if num_vis < num_to_vis:
+                # input raycast
+                grid2world = np.linalg.inv(sample['world2grid'])
+                cam_pos = np.matmul(grid2world, np.array([max_input_dim[2]//2, max_input_dim[1]//2, max_input_dim[0]*2, 1]))
+                cam_pos = torch.from_numpy(cam_pos).cuda()
+                camera_pose[:, :3, 3] = cam_pos[:, :3]
+
+                view_matrix = style.compute_view_matrix(sample['world2grid'].cuda(), camera_pose)
+                input_locs = torch.nonzero(torch.abs(inputs[:, 0]) < args.truncation)
+                input_locs = torch.cat([input_locs[:, 1:], input_locs[:, :1]], 1)
+                input_vals = inputs[input_locs[:, -1], :, input_locs[:, 0], input_locs[:, 1], input_locs[:, 2]]
+                input_normals = loss_util.compute_normals(inputs[:, :1], input_locs,
+                                                          transform=torch.inverse(view_matrix))
+                raycast_color, _, raycast_normal, _ = raycaster_rgbd(
+                    input_locs.cuda(), input_vals[:, :1].contiguous(), input_vals[:, 1:].contiguous(), input_normals,
+                    None, view_matrix, intrinsics)
+                if args.weight_color_loss > 0:
+                    invalid = raycast_color == -float('inf')
+                    input2d = raycast_color.clone() * 2 - 1
+                    input2d[invalid] = 0
+                normals = raycast_normal.clone()
+                invalid = raycast_normal == -float('inf')
+                normals[invalid] = 0
+                if args.weight_color_loss > 0:
+                    input2d = torch.cat([input2d, normals], 3)
+                else:
+                    input2d = normals
+                input2d = input2d.permute(0, 3, 1, 2).contiguous()
+
+                # target raycast
+                locs = torch.nonzero(torch.abs(target_for_sdfs[:, 0]) < args.truncation)
+                locs = torch.cat([locs[:, 1:], locs[:, :1]], 1).contiguous()
+                vals = target_for_sdfs[locs[:, -1], :, locs[:, 0], locs[:, 1], locs[:, 2]].contiguous()
+                colors = target_for_colors[locs[:, -1], locs[:, 0], locs[:, 1], locs[:, 2], :].float() / 255.0
+                target_normals = loss_util.compute_normals_sparse(locs, vals, target_for_sdfs.shape[2:],
+                                                                  transform=torch.inverse(view_matrix))
+                target_semantics = target_for_semantics[locs[:, -1], :, locs[:, 0], locs[:, 1], locs[:, 2]]
+                target_semantics_onehot = F.one_hot(target_semantics[:, 0].long())[..., :-1].float().contiguous()
+                raycast_color, _, raycast_normal, raycast_semantic = raycaster_rgbd(
+                    locs, vals, colors.contiguous(), target_normals, target_semantics_onehot, view_matrix, intrinsics)
+                if args.weight_color_loss > 0:
+                    invalid = raycast_color == -float('inf')
+                    target2d = raycast_color.clone() * 2 - 1
+                    target2d[invalid] = 0
+                invalid = raycast_normal == -float('inf')
+                normals = raycast_normal.clone()
+                normals[invalid] = 0
+                if args.weight_color_loss > 0:
+                    target2d = torch.cat([target2d, normals], 3)
+                else:
+                    target2d = normals
+                target2d = target2d.permute(0, 3, 1, 2).contiguous()
+                if args.weight_semantic_loss > 0:
+                    cat = torch.cat((raycast_semantic.clone(), torch.ones(raycast_semantic.shape[:-1] + (1,)).cuda()), dim=-1)
+                    _, target2d_label = torch.max(cat, dim=-1, keepdim=True)
+                    target2d_label = target2d_label.to(torch.uint8)
+
+                # prediction raycast
+                if args.weight_color_loss > 0:
+                    color = output_color
+                else:
+                    color = torch.zeros(output_sdf[0].shape[0], 3).cuda()
+                if args.weight_semantic_loss > 0:
+                    semantic = output_semantic.clone()
+                else:
+                    semantic = 41 * torch.ones(output_sdf[0].shape[:-1] + (41,)).cuda()
+                output_normals = loss_util.compute_normals_sparse(output_sdf[0].cuda(), output_sdf[1],
+                                                                  target_for_sdfs.shape[2:],
+                                                                  transform=torch.inverse(view_matrix))
+                raycast_color, raycast_depth, raycast_normal, raycast_semantic = raycaster_rgbd(
+                    output_sdf[0].cuda(), output_sdf[1], color, output_normals, semantic, view_matrix, intrinsics)
+                if args.weight_color_loss > 0:
+                    raycast = torch.cat([raycast_color, raycast_normal], 3)
+                else:
+                    raycast = raycast_normal
+                synth = raycast.permute(0, 3, 1, 2).contiguous()
+                invalid = synth == -float('inf')
+                synth[:, :3] = synth[:, :3] * 2 - 1  # normalize
+                synth[invalid] = 0
+                if args.weight_semantic_loss > 0:
+                    cat = torch.cat((raycast_semantic.detach(), torch.ones(raycast_semantic.shape[:-1] + (1,)).cuda()),
+                                    dim=-1)
+                    _, pred2d_label = torch.max(cat, dim=-1, keepdim=True)
+                    pred2d_label = pred2d_label.to(torch.uint8)
+
                 vis_pred_sdf = [None]
                 vis_pred_color = [None]
                 vis_pred_semantic = [None]
@@ -162,14 +286,38 @@ def test(dataloader, output_vis, num_to_vis):
                         vis_pred_semantic[0] = output_semantic.detach().cpu().numpy()
                 vis_pred_images_color = None
                 vis_tgt_images_color = None
+                vis_input_images_color = None
+                vis_pred_depth = None
+                vis_target_depth = None
                 vis_pred_images_semantic = None
                 vis_target_images_semantic = None
+
+                if input2d is not None:
+                    vis_input_images_color = input2d.cpu().numpy()
+                    if args.weight_color_loss > 0:
+                        vis_input_images_color[:, :3] = (vis_input_images_color[:, :3] + 1) * 0.5
+                    vis_input_images_color = np.transpose(vis_input_images_color, [0, 2, 3, 1])
+                if target2d is not None:
+                    if output_color is not None:
+                        synth[:, :3] = (synth[:, :3] + 1) * 0.5
+                        target2d[:, :3] = (target2d[:, :3] + 1) * 0.5
+                    vis_pred_images_color = synth.detach().cpu().numpy()
+                    vis_pred_images_color = np.transpose(vis_pred_images_color, [0, 2, 3, 1])
+                    vis_tgt_images_color = target2d.cpu().numpy()
+                    vis_tgt_images_color = np.transpose(vis_tgt_images_color, [0, 2, 3, 1])
+
+                if pred2d_label is not None:
+                    vis_pred_images_semantic = pred2d_label.cpu().numpy()
+                    vis_target_images_semantic = target2d_label.cpu().numpy()
+
                 data_util.save_predictions(output_vis, np.arange(1), sample['name'], inputs.cpu().numpy(),
-                                           target_for_sdf.cpu().numpy(), target_for_colors.cpu().numpy(),
-                                           target_for_semantics, None, vis_tgt_images_color, vis_target_images_semantic,
-                                           vis_pred_sdf, vis_pred_color, vis_pred_semantic, None, vis_pred_images_color,
-                                           vis_pred_images_semantic, sample['world2grid'], args.truncation,
-                                           np.load("category_color.npz")['mapping_color'], args.color_space)
+                                           target_for_sdfs.cpu().numpy(), target_for_colors.cpu().numpy(),
+                                           target_for_semantics.cpu().numpy(), None, vis_tgt_images_color,
+                                           vis_target_images_semantic, vis_pred_sdf, vis_pred_color, vis_pred_semantic,
+                                           None, vis_pred_images_color, vis_pred_images_semantic, sample['world2grid'],
+                                           args.truncation, np.load("category_color.npz")['mapping_color'],
+                                           args.color_space, input_images=vis_input_images_color,
+                                           pred_depth=vis_pred_depth, target_depth=vis_target_depth)
                 num_vis += 1
             gc.collect()
     sys.stdout.write('\n')
@@ -198,9 +346,9 @@ def main():
     if os.path.exists(args.output):
         if args.vis_only:
             print('warning: output dir %s exists, will overwrite any existing files')
-        # else:
-        #     input('warning: output dir %s exists, press key to delete and continue' % args.output)
-        #     shutil.rmtree(args.output)
+        else:
+            input('warning: output dir %s exists, press key to delete and continue' % args.output)
+            shutil.rmtree(args.output)
     if not os.path.exists(args.output):
         os.makedirs(args.output)
     output_vis_path = os.path.join(args.output, 'vis')
